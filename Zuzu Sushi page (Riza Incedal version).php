29ed6d3f77e9faf99bb907fe57dfd7db99311129
rincedal <94069199+rincedal@users.noreply.github.com>
2022-11-01 13:21:09 +0100

    Add files via upload

--- Zuzu Sushi page (Riza Incedal version).php
+++ Zuzu Sushi page (Riza Incedal version).php
@@ -15,27 +15,27 @@ $error = "";
 try {
     $database = new PDO("mysql:host=$servername;dbname=$dbname", $username);
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $makiCucumber = $database->prepare("SELECT * FROM sushi WHERE `ID` = 1;");
+    $makiCucumber = $database->prepare("SELECT * FROM sushi WHERE `id` = 1;");
     $makiCucumber->execute();
 
     $mcResult = $makiCucumber->fetchAll(PDO::FETCH_ASSOC);
-    $makiAvocado = $database->prepare("SELECT * FROM sushi WHERE `ID` = 2;");
+    $makiAvocado = $database->prepare("SELECT * FROM sushi WHERE `id` = 2;");
     $makiAvocado->execute();
 
     $maResult = $makiAvocado->fetchAll(PDO::FETCH_ASSOC);
-    $nigiriSalmon = $database->prepare("SELECT * FROM sushi WHERE `ID` = 3;");
+    $nigiriSalmon = $database->prepare("SELECT * FROM sushi WHERE `id` = 3;");
     $nigiriSalmon->execute();
 
     $nsResult = $nigiriSalmon->fetchAll(PDO::FETCH_ASSOC);
-    $philadelphiaRoll = $database->prepare("SELECT * FROM sushi WHERE `ID` = 4;");
+    $philadelphiaRoll = $database->prepare("SELECT * FROM sushi WHERE `id` = 4;");
     $philadelphiaRoll->execute();
 
     $prResult = $philadelphiaRoll->fetchAll(PDO::FETCH_ASSOC);
-    $spicyTunaRoll = $database->prepare("SELECT * FROM sushi WHERE `ID` = 5;");
+    $spicyTunaRoll = $database->prepare("SELECT * FROM sushi WHERE `id` = 5;");
     $spicyTunaRoll->execute();
 
     $strResult = $spicyTunaRoll->fetchAll(PDO::FETCH_ASSOC);
-    $californiaRoll = $database->prepare("SELECT * FROM sushi WHERE `ID` = 6;");
+    $californiaRoll = $database->prepare("SELECT * FROM sushi WHERE `id` = 6;");
     $californiaRoll->execute();
 
     $crResult = $californiaRoll->fetchAll(PDO::FETCH_ASSOC);
@@ -137,110 +137,124 @@ try {
         ?>
 
         <img src= "/Images/MakiCucumber.jpg" class= "position-relative formImage1">
-        <div class="col-md-4 position-relative input1">
-            <label for="validationCustom01" class="form-label"><?php echo $column1['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom01" required name= "makiCucumber" value= "0">
-        </div>
 
-            <?php } ?>
+            <div class="col-md-4 position-relative input1">
+                <label for="validationCustom01" class="form-label"><?php echo $column1['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                <input type="text" class="form-control" id="validationCustom01" required name= "makiCucumber" value= "0">
+            </div>
+
+        <?php } ?>
 
         <?php foreach($maResult as $column2) { ?>
 
         <img src= "/Images/MakiAvocado.jpg" class= "position-relative formImage2">
-        <div class="col-md-4 position-relative input2">
-            <label for="validationCustom02" class="form-label"><?php echo $column2['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom02" required name= "makiAvocado" value= "0">
-        </div>
 
-            <?php } ?>
+            <div class="col-md-4 position-relative input2">
+                <label for="validationCustom02" class="form-label"><?php echo $column2['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                <input type="text" class="form-control" id="validationCustom02" required name= "makiAvocado" value= "0">
+            </div>
+
+        <?php } ?>
 
         <?php foreach($nsResult as $column3) { ?>
 
         <img src= "/Images/NigiriSalmon.jpg" class= "position-relative formImage3">
-        <div class="col-md-6 position-relative input3">
-            <label for="validationCustom04" class="form-label"><?php echo $column3['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom03" required name="nigiriSalmon" value= "0">
-        </div>
+
+            <div class="col-md-4 position-relative input3">
+                <label for="validationCustom02" class="form-label"><?php echo $column3['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                <input type="text" class="form-control" id="validationCustom02" required name= "nigiriSalmon" value= "0">
+            </div>
 
             <?php } ?>
 
             <?php foreach($prResult as $column4) { ?>
 
         <img src= "/Images/PhiladelphiaRoll.jpg" class= "position-relative formImage4">
-        <div class="col-md-6 position-relative input4">
-            <label for="validationCustom04" class="form-label"><?php echo $column4['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom03" required name= "philadelphiaRoll" value= "0">
-        </div>
 
-        <?php } ?>
+                <div class="col-md-6 position-relative input4">
+                    <label for="validationCustom04" class="form-label"><?php echo $column3['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                    <input type="text" class="form-control" id="validationCustom03" required name="philadelphiaRoll" value= "0">
+                </div>
+
+            <?php } ?>
 
         <?php foreach ($strResult as $column5) { ?>
 
         <img src= "/Images/SpicyTunaRoll.jpg" class= "position-relative formImage5">
-        <div class="col-md-3 position-relative input5">
-            <label for="validationCustom05" class="form-label"><?php echo $column5['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom03" required name="spicyTunaRoll" value= "0">
-        </div>
+
+            <div class="col-md-3 position-relative input5">
+                <label for="validationCustom05" class="form-label"><?php echo $column5['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                <input type="text" class="form-control" id="validationCustom03" required name="spicyTunaRoll" value= "0">
+            </div>
 
             <?php } ?>
 
         <?php foreach($crResult as $column6) { ?>
 
-
+            <div class="col-md-3 position-relative input6">
+                <label for="validationCustom06" class="form-label"><?php echo $column6['name']; ?>&nbsp;<i>(20 pieces)</i></label>
+                <input type="text" class="form-control" id="validationCustom05" required name= "californiaRoll" value= "0">
+            </div>
 
         <img src= "/Images/CaliforniaRoll.jpg" class= "position-relative formImage6">
-        <div class="col-md-3 position-relative input6">
-            <label for="validationCustom06" class="form-label"><?php echo $column6['Name']; ?>&nbsp;<i>(20 pieces)</i></label>
-            <input type="text" class="form-control" id="validationCustom05" required name= "californiaRoll" value= "0">
-        </div>
-
-
-            <div class="col-12 w-100">
-            <button type="submit" class="btn btn-dark button button2 position-relative" name="subButton1">Continue</button>
-        </div>
+            </div>
 
         <?php } ?>
 
-    </form>
-    </div>
-    </div>
-    <!--Footer-->
-    <footer class="bg-dark text-center text-white footer">
-        <div class="container p-4">
-            <section class="">
-                <form action="">
-                    <div class="row d-flex justify-content-center">
-                    </div>
-                </form>
-            </section>
-            <section class="">
-                <div class="row">
-                    <div class="col">
-                        <p><b>Contact</b><br>
-                            Zuzu <br>
-                            Kalealtı Caddesi 63,<br>
-                            63420 <a class= "link1" href="https://en.wikipedia.org/wiki/Birecik">Birecik</a> <br>
-                            zuzu.birecik@gmail.com <br>
-                            +904141516151</p>
-                    </div>
+        </form>
+        </div>
+        </div>
+        <div class="col-12 w-100">
+        <button type="submit" class="btn btn-dark button button2 position-relative" name="subButton1">Continue</button>
 
-                    <div class= "col"><b>Opening hours</b> <br>
-                        Monday: 09:00 - 00:00 <br>
-                        Tuesday: 09:00 - 00:00 <br>
-                        Wednesday: 09:00 - 00:00 <br>
-                        Thursday: 09:00 - 00:00 <br>
-                        Friday: 09:00 - 00:00 <br>
-                        Saturday: 09:00 - 00:00 <br>
-                        Sunday: 09:00 - 00:00 <br>
-                    </div>
+            <!--Footer-->
+            <footer class="bg-dark text-center text-white footer">
+                <div class="container p-4">
+                    <section class="">
+                        <form action="">
+                            <div class="row d-flex justify-content-center">
+                            </div>
+                        </form>
+                    </section>
+                    <section class="">
+                        <div class="row">
+                            <div class="col">
+                                <p><b>Contact</b><br>
+                                    Zuzu <br>
+                                    Kalealtı Caddesi 63,<br>
+                                    63420 <a class= "link1" href="https://en.wikipedia.org/wiki/Birecik">Birecik</a> <br>
+                                    zuzu.birecik@gmail.com <br>
+                                    +904141516151</p>
+                            </div>
+
+                            <div class= "col"><b>Opening hours</b> <br>
+                                Monday: 09:00 - 00:00 <br>
+                                Tuesday: 09:00 - 00:00 <br>
+                                Wednesday: 09:00 - 00:00 <br>
+                                Thursday: 09:00 - 00:00 <br>
+                                Friday: 09:00 - 00:00 <br>
+                                Saturday: 09:00 - 00:00 <br>
+                                Sunday: 09:00 - 00:00 <br>
+                            </div>
+                        </div>
+                    </section>
                 </div>
-            </section>
-        </div>
 
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            <p class="text-white" > Copyright © 2022  Riza Incedal</p>
-        </div>
-    </footer>
+                <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                    <p class="text-white" > Copyright © 2022  Riza Incedal</p>
+                </div>
+            </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
+
+
+
+
+
+
+
+
+
+
+
